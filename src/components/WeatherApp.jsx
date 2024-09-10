3136973361313c809c537a8a728ff938123523db
Marshal10 <marshalmaleane123@gmail.com>
2024-09-10 20:38:51 +0530

    Search weather from input

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -5,19 +5,34 @@ import rainy from "../assets/images/rainy.png";
 import snowy from "../assets/images/snowy.png";
 
 import "./WeatherApp.css";
+import { useState } from "react";
 
 const apiKey = import.meta.env.VITE_WEATHER_FORECAST_API;
 
 function WeatherApp() {
+  const [searchLoc, setSearchLoc] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+  const [error, setError] = useState("");
+
   async function fetchWeather() {
-    try {
-      const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=Bangalore&units=Metric&appid=${apiKey}`
-      );
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.error(err.message);
+    if (!searchLoc) return;
+
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchLoc}&units=Metric&appid=${apiKey}`
+    );
+    const data = await res.json();
+    console.log(data);
+    if (data.cod !== 200) {
+      setError(data.message);
+    } else {
+      setError("");
+      setWeatherData(data);
+    }
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      fetchWeather();
     }
   }
 
@@ -25,38 +40,50 @@ function WeatherApp() {
     <div className="container">
       <div className="weather-app">
         <div className="search">
-          <div className="search-top">
-            <i className="fa-solid fa-location-dot"></i>
-            <div className="location">Bangalore</div>
-          </div>
+          {weatherData?.name && (
+            <div className="search-top">
+              <i className="fa-solid fa-location-dot"></i>
+              <div className="location">{weatherData.name}</div>
+            </div>
+          )}
           <div className="search-bar">
-            <input type="text" placeholder="Enter Location" />
+            <input
+              type="text"
+              placeholder="Enter Location"
+              value={searchLoc}
+              onChange={(e) => setSearchLoc(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
             <i
               className="fa-solid fa-magnifying-glass"
               onClick={fetchWeather}
             ></i>
           </div>
         </div>
-        <div className="weather">
-          <img src={sunny} alt="sunny" />
-          <div className="weather-type">Clear</div>
-          <div className="temp">28°</div>
-          <div className="weather-date">
-            <p>Mon, 9 Sep</p>
-          </div>
-          <div className="weather-data">
-            <div className="humidity">
-              <div className="data-name">Humidity</div>
-              <i className="fa-solid fa-droplet"></i>
-              <div className="data">55%</div>
+        {error ? (
+          <div className="error">{error}</div>
+        ) : (
+          <div className="weather">
+            <img src={sunny} alt="sunny" />
+            <div className="weather-type">Clear</div>
+            <div className="temp">28°</div>
+            <div className="weather-date">
+              <p>Mon, 9 Sep</p>
             </div>
-            <div className="wind">
-              <div className="data-name">Wind</div>
-              <i className="fa-solid fa-wind"></i>
-              <div className="data">3 km/hr</div>
+            <div className="weather-data">
+              <div className="humidity">
+                <div className="data-name">Humidity</div>
+                <i className="fa-solid fa-droplet"></i>
+                <div className="data">55%</div>
+              </div>
+              <div className="wind">
+                <div className="data-name">Wind</div>
+                <i className="fa-solid fa-wind"></i>
+                <div className="data">3 km/hr</div>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
